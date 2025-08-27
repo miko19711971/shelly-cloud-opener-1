c40789867293b34b1561affa15ce79313322bcdb
miko19711971 <mikbondi@gmail.com>
2025-08-27 12:47:51 +0200

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,14 @@
+{
+  "name": "shelly-cloud-opener",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "src/server.js",
+  "scripts": {
+    "start": "node src/server.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.7",
+    "crypto": "^1.0.1",
+    "express": "^4.18.2"
+  }
+}
