cdf0d210e05280b1fef894a21189737b99e7aacf
miko19711971 <mikbondi@gmail.com>
2025-11-23 20:43:28 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ app.set("trust proxy", true);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // ========= STATIC PATHS =========
 const __filename = fileURLToPath(import.meta.url);
