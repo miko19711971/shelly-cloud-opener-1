614e9c93e25fae0bb96862be1316549802369493
miko19711971 <mikbondi@gmail.com>
2026-01-03 00:30:45 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.disable("x-powered-by");
 app.set("trust proxy", true);
 app.use(express.urlencoded({ extended: true, limit: "50kb" }));
 app.use(express.json({ limit: "100kb" }));
-app.post("/feedback", async (req, res) => {
+app.post("/feedback", cors(), async (req, res) => {
   console.log("FEEDBACK ARRIVATO", req.body);
   try {
     await fetch(
