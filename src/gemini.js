d26caacc8d10a2918433f6cc335628b3e4ca960a
miko19711971 <mikbondi@gmail.com>
2026-01-20 18:50:05 +0100

    Update gemini.js

--- src/gemini.js
+++ src/gemini.js
@@ -20,7 +20,7 @@ export async function askGemini({
     const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
     const model = genAI.getGenerativeModel({
-      model: "models/gemini-1.5-pro"
+      model: "models/gemini-1.5-flash"
     });
 
     const prompt = `
