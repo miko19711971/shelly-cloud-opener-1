6fe419490d1e3f9a84713518920fe9e28662024e
miko19711971 <mikbondi@gmail.com>
2026-01-22 18:45:20 +0100

    Update recruiting-mailer.js

--- src/recruiting/recruiting-mailer.js
+++ src/recruiting/recruiting-mailer.js
@@ -1,1 +1,17 @@
+import sgMail from "@sendgrid/mail";
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export async function sendEmail({ to, subject, text }) {
+  if (!to) throw new Error("Missing recipient");
+
+  const msg = {
+    to,
+    from: process.env.MAIL_FROM,
+    replyTo: process.env.MAIL_REPLY_TO,
+    subject,
+    text
+  };
+
+  await sgMail.send(msg);
+}
