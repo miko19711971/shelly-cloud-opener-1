656a00ebb7b233fb9d575e5d5d212c3d46499059
miko19711971 <mikbondi@gmail.com>
2026-03-05 21:36:48 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1920,7 +1920,18 @@ app.post('/allegria-info', async (req, res) => {
   try {
     const email = req.body?.email;
     if (!email) return res.status(400).send('Email mancante');
-
+// salva lead su Google Sheet
+await axios.post(
+  "https://script.google.com/macros/s/AKfycbzsuNiIXjdnWMuRocDkpqCU4c-4sUlwVMplebibQGaPFMIVF0sE41QKjsldlMVthH-CbA/exec",
+  {
+    email: email,
+    source: "Landing Allegria"
+  },
+  {
+    headers: { "Content-Type": "application/json" },
+    timeout: 10000
+  }
+);
      const htmlBody = `
   <p>Grazie per l'interesse ad Allegria.</p>
   <p>Allegria offre presenza e compagnia a domicilio per anziani autosufficienti.</p>
